2162ee23ee91fd4683b4cc262992d78257d5f7d0
DaviFPassos <davvidpassos@gmail.com>
2026-05-26 12:24:04 -0300

    chore: optimize monorepo root gitignore rules for multi-stack development

--- gateway-service-go/main.go
+++ gateway-service-go/main.go
@@ -2,15 +2,23 @@ package main
 
 import (
 	"bytes"
+	"context"
 	"encoding/json"
 	"io"
+	"log"
 	"net/http"
 	"os"
 	"strings"
 	"time"
 
 	"github.com/gin-gonic/gin"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
+
+var mongoCollection *mongo.Collection
 
 // getMLServiceURL retrieves the ML service endpoint from environment variables
 // Defaults to localhost:8000 for development, but allows override via ML_SERVICE_URL env var
@@ -21,7 +29,12 @@ func getMLServiceURL() string {
     return "http://localhost:8000/v1/predict"
 }
 
-
+func getMongoURI() string {
+	if uri:= os.Getenv("MONGO_URI"); uri != "" {
+		return uri
+	}
+	return "mongo;//localhost:27017"
+}
 
 // ReviewRequest represents the JSON payload sent by clients to this API
 // The binding:"required" tag ensures the field is validated and non-empty
@@ -37,10 +50,35 @@ type MLResponse struct {
 	Confidence     float64 `json:"confidence"`
 }
 
+// initMongoDB to initialize database connection
+
+func initMongoDB() {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	clientOptions := options.Client().ApplyURI(getMongoURI())
+	client, err := mongo.Connect(ctx, clientOptions)
+	if err != nil {
+		log.Fatalf("Failed to connnect to MOngoDB: %v", err)
+	}
+
+	// Look if connection is actually activated (PING)
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		log.Fatalf("MongoDB ping faield: %v", err)
+	}
+
+	// Defines database 'restaurant_db' and collection 'reviews'
+	mongoCollection = client.Database("restaurant_db").Collection("reviews")
+	log.Println("Successfully connected to MongoDB!")
+}
+
 func main() {
+	// Initialize databank before upload API
+	initMongoDB()
+
 	// Set Gin framework to release mode for maximum performance and minimal logging
 	gin.SetMode(gin.ReleaseMode)
-
 	r := gin.Default()
 
 	r.Use(func(c *gin.Context) {
@@ -111,6 +149,25 @@ func main() {
 			c.JSON(http.StatusInternalServerError, gin.H{"error": "Error parsing AI prediction."})
 			return
 		}
+		
+		// Asynchronous Record on MOngoDB
+		go func (text string, res MLResponse)  {
+			dbCtx, dbCancel := context.WithTimeout(context.Background(), 2*time.Second)
+			defer dbCancel()
+
+			document := bson.M{
+				"review_text": text,
+				"prediction_code": res.PredictionCode,
+				"sentiment": res.Sentiment,
+				"confidence": res.Confidence,
+				"created_at": time.Now(),
+			}
+			
+			_, insertErr := mongoCollection.InsertOne(dbCtx, document)
+			if insertErr != nil {
+				log.Printf("[MLOps Error] Failed to log review to MongoDB: %v", insertErr)
+			}
+		}(textClean, mlResponse)
 
 		// Step 7: Return successful response with prediction data
 		c.JSON(http.StatusOK, gin.H{
